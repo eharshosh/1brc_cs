94010a15ff0b784113b4d424f34a6dc3573aad5d
Eitan Har-Shoshanim <eitan@solargik.com>
2024-12-06 16:17:44 +0200

    wip

--- Program.cs
+++ Program.cs
@@ -2,93 +2,107 @@
 using System.Text;
 using System.Text.RegularExpressions;
 
- const string dataFilename = "/Users/eitan/development/1brc.data/measurements-1000000000.txt";
- const string compareToFilename = "/Users/eitan/development/1brc.data/measurements-1000000000.out";
+namespace _1brc;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        const string dataFilename = "/Users/eitan/development/1brc.data/measurements-1000000000.txt";
+        const string compareToFilename = "/Users/eitan/development/1brc.data/measurements-1000000000.out";
+
 // const string dataFilename = "/Users/eitan/development/1brc.data/measurements-10000000.txt";
 // const string compareToFilename = "/Users/eitan/development/1brc.data/measurements-10000000.out";
 
-using var fs = File.OpenRead(dataFilename);
-using var reader = new StreamReader(fs, Encoding.UTF8);
+        var sw = Stopwatch.StartNew();
+        var map = new Dictionary<string, CityState>();
+        var alternateLookup = map.GetAlternateLookup<ReadOnlySpan<char>>();
+        var lineCount = 0;
 
-var rawInput = File.ReadAllText(compareToFilename)[1..^2];
-var compareTo = Regex
-    .Split(rawInput, @"([\w\s\(\),\.'\-]+)=([\-\.\d]+)/([\-\.\d]+)/([\-\.\d]+)(, )?", RegexOptions.CultureInvariant)
-    .Chunk(6)
-    .Select(chunk => new OutputLine(chunk[1], float.Parse(chunk[2]), float.Parse(chunk[3]), float.Parse(chunk[4])))
-    .ToDictionary(x => x.City);
+        using var fs = File.OpenRead(dataFilename);
+        using var reader = new StreamReader(fs, Encoding.UTF8);
 
-var sw = Stopwatch.StartNew();
-var map = new Dictionary<string, CityState>();
-var alternateLookup = map.GetAlternateLookup<ReadOnlySpan<char>>();
-var lineCount = 0;
+        while (reader.ReadLine() is { } line)
+        {
+            if (++lineCount % 100_000_000 == 0)
+            {
+                Console.WriteLine($"{lineCount}@{sw.Elapsed}");
+            }
 
-while (reader.ReadLine() is { } line)
-{
-    if (++lineCount % 100_000_000 == 0)
-    {
-        Console.WriteLine($"{lineCount}@{sw.Elapsed}");
+            var lineSpan = line.AsSpan();
+            var semiColonIndex = lineSpan.IndexOf(';');
+            var city = lineSpan[..semiColonIndex];
+            var measurement = lineSpan[(semiColonIndex + 1)..];
+            var dotIndex = measurement.IndexOf('.');
+            long factor = (int)Math.Pow(10, measurement.Length - dotIndex);
+            var signFactor = measurement[0] == '-' ? -1 : 1;
+            var value = (int)(int.Parse(measurement[..dotIndex]) * 1000
+                                + int.Parse(measurement[(dotIndex + 1)..]) * factor * signFactor);
+            if (!alternateLookup.TryGetValue(city, out var state))
+            {
+                state = new CityState { Min = value, Max = value, Sum = value, Count = 1 };
+                alternateLookup[city] = state;
+                continue;
+            }
+
+            state.Min = Math.Min(state.Min, value);
+            state.Max = Math.Max(state.Max, value);
+            state.Sum += value;
+            state.Count++;
+        }
+
+        var outputName = Path.GetFileNameWithoutExtension(dataFilename) + "_ours.out";
+        var outFile = File.OpenWrite(outputName);
+        var writer = new StreamWriter(outFile);
+        var output = new List<OutputLine>(map.Keys.Count);
+        foreach (var (key, state) in map.OrderBy(kvp => kvp.Key))
+        {
+            var avg = Math.Round((double)state.Sum / state.Count / 100) / 10;
+            var min = Math.Round((double)state.Min / 100) / 10;
+            var max = Math.Round((double)state.Max / 100) / 10;
+            writer.WriteLine(key + ";" + min + ";" + avg + ";" + max);
+            output.Add(new(key, (float)min, (float)avg, (float)max));
+        }
+
+        Console.WriteLine(sw.Elapsed);
+
+
+        var outputSorted = output.ToDictionary(x => x.City);
+        var compareTo = ReadCompareTo(compareToFilename);
+        foreach (var (city, item) in compareTo)
+        {
+            if (!outputSorted.TryGetValue(city, out var outputItem))
+            {
+                Console.WriteLine(city + " not found");
+                continue;
+            }
+
+            if (item != outputItem)
+            {
+                Console.WriteLine($"{city}: {item} != {outputItem}");
+            }
+        }
     }
 
-    var lineSpan = line.AsSpan();
-    var semiColonIndex = lineSpan.IndexOf(';');
-    var city = lineSpan[..semiColonIndex];
-    var measurement = lineSpan[(semiColonIndex + 1)..];
-    var dotIndex = measurement.IndexOf('.');
-    long factor = (int)Math.Pow(10, measurement.Length - dotIndex);
-    var signFactor = measurement[0] == '-' ? -1 : 1;
-    var value = long.Parse(measurement[..dotIndex]) * 1000
-                + long.Parse(measurement[(dotIndex + 1)..]) * factor * signFactor;
-    if (!alternateLookup.TryGetValue(city, out var state))
+    static Dictionary<string, OutputLine> ReadCompareTo(string filename)
     {
-        state = new CityState { min = value, max = value, sum = value, count = 1 };
-        alternateLookup[city] = state;
-        continue;
+        var rawInput = File.ReadAllText(filename)[1..^2];
+        return Regex
+            .Split(rawInput, @"([\w\s\(\),\.'\-]+)=([\-\.\d]+)/([\-\.\d]+)/([\-\.\d]+)(, )?",
+                RegexOptions.CultureInvariant)
+            .Chunk(6)
+            .Select(chunk =>
+                new OutputLine(chunk[1], float.Parse(chunk[2]), float.Parse(chunk[3]), float.Parse(chunk[4])))
+            .ToDictionary(x => x.City);
     }
 
-    state.min = Math.Min(state.min, value);
-    state.max = Math.Max(state.max, value);
-    state.sum += value;
-    state.count++;
-}
-
-var outputName = Path.GetFileNameWithoutExtension(dataFilename) + "_ours.out";
-var outFile = File.OpenWrite(outputName);
-var writer = new StreamWriter(outFile);
-var output = new List<OutputLine>(map.Keys.Count);
-foreach (var (key, state) in map.OrderBy(kvp => kvp.Key))
-{
-    var avg = Math.Round((double)state.sum / state.count / 100) / 10;
-    var min = Math.Round((double)state.min / 100) / 10;
-    var max = Math.Round((double)state.max / 100) / 10;
-    writer.WriteLine(key + ";" + min + ";" + avg + ";" + max);
-    output.Add(new(key, (float)min, (float)avg, (float)max));
-}
-
-Console.WriteLine(sw.Elapsed);
-
-
-var outputSorted = output.ToDictionary(x => x.City);
-
-foreach (var (city, item) in compareTo)
-{
-    if (!outputSorted.TryGetValue(city, out var outputItem))
+    class CityState
     {
-        Console.WriteLine(city + " not found");
-        continue;
+        public int Min;
+        public long Sum;
+        public int Max;
+        public int Count;
     }
 
-    if (item != outputItem)
-    {
-        Console.WriteLine($"{city}: {item} != {outputItem}");
-    }
+    record OutputLine(string City, float Min, float Average, float Max);
 }
-
-class CityState
-{
-    public long min;
-    public long sum;
-    public long max;
-    public long count;
-}
-
-record OutputLine(string City, float Min, float Average, float Max);
